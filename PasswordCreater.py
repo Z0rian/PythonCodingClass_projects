23c589b1d294d60315628fcb57fd88a1b6ce930a
Z0rian <zorian@nerol.net>
2020-10-28 11:59:08 -0700

    I added some better user experiance things, like better prompts and better experiance in the options.

--- PasswordCreater.py
+++ PasswordCreater.py
@@ -4,21 +4,32 @@ Created on Wed Sep 23 11:29:48 2020
 
 @author: zorian
 """
+
+
 #asking for how many passwords it will spit out, and how long the random chars should be.
 import random
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!$%^&*,.<>/     1234567890'
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!$%^&*,.<>/ 1234567890'
     
+
+print('\nHello! Welcome to THE BEST Password Generator... I will prompt you with some questions, which will determine what your password looks like.\n')
+print('To get started, some good awnsers are "10", for the number of passwords,')
+print('"3" for the # of random charectars,')
+print('"5" for how many words to choose from,')
+print('and then words like your name, your pets name, and farmiliar words or phrases')
+print('Here we go!')
 passnum = input('How many passwords would you like to create?')
 passnum = int(passnum)
 
-passlen = input('How many (random) charectars should the password(s) be?')
+#passlen = input('How many (random) charectars should the password(s) be?')
+#try:
+#    passlen = int(passlen)
+#except ValueError:
+#    print('Please put an intiger')
+#    
+passlen = input('How many (random) charectars should the password(s) have?')
 passlen = int(passlen)
-
-
-#creating random chars passnum number of times...
-#loopsleft = 5
-
+    
 
 def randchars():
     randomsegment = ''
@@ -35,8 +46,6 @@ def leetify(inputword):
     leetword = leetword.replace('i', '1')
     leetword = leetword.replace('b', '8')
     leetword = leetword.replace('s', '5')
-    leetword = leetword.replace('l', '1')
-    leetword = leetword.replace('a', '4')
     leetword = leetword.replace('O', '0')
     leetword = leetword.replace('E', '3')
     leetword = leetword.replace('I', '1')
@@ -59,7 +68,7 @@ loopsleft = int(loopsleft)
 
 #populating keywords:
 while loopsleft > 0:
-    word = input(f'What should the part of the password that you already know be? Pet name, favorate food? {loopsleft} left.')
+    word = input(f'What should the part of the password that you already know be? Number seaquence? {loopsleft} left.')
     keywords.append(word)
     keywords.append(leetify(word))
     loopsleft -= 1
